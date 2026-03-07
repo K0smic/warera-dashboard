64797d42a9330d59bad90d3aeb56f601c2ec62f2
K0smic <ercuta.elian@gmail.com>
2026-03-07 19:27:48 +0100

    cleaned gh-pages

--- src/lib/components/molecules/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as CompanyHeader } from './company-header.svelte';
-export { default as SummaryCards } from './company-summary-cards.svelte';
-export { default as CompanyPerformanceCard } from './company-performance-card.svelte';
-export { default as UpgradesCard } from './company-upgrades-card.svelte';
-export { default as BestRegionsTable } from './company-region-tables.svelte';
-export { default as CompanyWage } from './company-wage.svelte';
-export { default as CompanyWorkers } from './company-workers.svelte';
