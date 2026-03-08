205cb2894b43931f7478e561f7e7756d69ee2c5d
K0smic <ercuta.elian@gmail.com>
2026-03-08 15:38:24 +0100

    added glitched version type

--- src/lib/components/molecules/header.svelte
+++ src/lib/components/molecules/header.svelte
@@ -4,10 +4,13 @@
 </script>
 
 <header
-	class="sticky top-0 z-10 flex h-16 shrink-0 items-center gap-2 border-b bg-background transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12"
+	class="sticky top-0 z-10 flex h-16 shrink-0 items-center justify-between gap-2 border-b bg-background px-4 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12"
 >
 	<div class="flex items-center gap-2 px-4">
 		<Sidebar.Trigger class="-ms-1" title="Toggle sidebar" />
 		<Separator orientation="vertical" class="me-2 data-[orientation=vertical]:h-4" />
+	</div>
+	<div>
+		<span class="glitch text-2xl font-bold text-destructive" data-text="ALPHA">ALPHA</span>
 	</div>
 </header>
