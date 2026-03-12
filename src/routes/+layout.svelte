741d19c295efb27a6075d22cf0109c3eed0205cc
K0smic <ercuta.elian@gmail.com>
2026-03-12 19:36:46 +0100

    added icons and logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import favicon96 from '$lib/assets/favicon-96x96.png';
+	import faviconIco from '$lib/assets/favicon.ico';
+	import faviconTouch from '$lib/assets/apple-touch-icon.png';
+	import manifest from '$lib/assets/site.webmanifest';
 
 	import * as Sidebar from '$lib/components/atoms/sidebar/index.js';
 	import AppSidebar from '$lib/components/organisms/sidebar.svelte';
@@ -11,7 +15,14 @@
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" type="image/png" href={favicon96} sizes="96x96" />
+	<link rel="icon" href={favicon} />
+	<link rel="shortcut icon" href={faviconIco} />
+	<link rel="apple-touch-icon" sizes="180x180" href={faviconTouch} />
+	<link rel="manifest" href={manifest} />
+</svelte:head>
+
 <Sidebar.Provider>
 	<ModeWatcher />
 	<AppSidebar />
