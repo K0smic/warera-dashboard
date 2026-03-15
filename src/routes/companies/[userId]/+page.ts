0dfd956b845732deec389503c04087f82e5b6d35
K0smic <ercuta.elian@gmail.com>
2026-03-15 15:04:30 +0100

    optimized api calls using batch mode

--- src/routes/companies/[userId]/+page.ts
+++ src/routes/companies/[userId]/+page.ts
@@ -1,11 +1,16 @@
 import type { PageLoad } from './$types';
-import { getCompaniesId, getCompany } from '$lib/services';
+import { getCompaniesId, batchFetch } from '$lib/services';
 
 export const load: PageLoad = async ({ fetch, params }) => {
 	const fetchedCompanies = await getCompaniesId({ userId: params.userId }, fetch);
 	const companiesIds = fetchedCompanies.items;
-	const companies = await Promise.all(
-		companiesIds.map((companyId) => getCompany({ companyId }, fetch))
+
+	const companies = await batchFetch(
+		companiesIds.map((companyId) => ({
+			path: 'company.getById',
+			input: { companyId }
+		})),
+		fetch
 	);
 
 	return { companies };
